bc68600683c7e27f7fbc831256e13f02f18a2e24
Dewke <dewke17@gmail.com>
2021-05-04 18:36:47 +0200

    Initial layout

--- src/App.js
+++ src/App.js
@@ -3,6 +3,41 @@ import './App.scss';
 
 export default class App extends Component {
   render() {
-    return <h3>Hello World!</h3>;
+    return (
+      <div className='container mt-5'>
+        {/* todo: remove border-right */}
+        <aside className='sidebar float-left border-right px-3'>
+          <div className='note-search-container d-flex justify-content-between my-4'>
+            <input type='text' className='form-control note-search-input' />
+            <button
+              type='button'
+              className='btn btn-outline-primary note-search-btn'
+            >
+              Search
+            </button>
+          </div>
+          <h5 className>All Notfdes</h5>
+          <ul className='list-unstyled notes-list'>
+            <li>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et
+              inventore ducimus dolores assumenda!
+            </li>
+            <li>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et
+              inventore ducimus dolores assumenda!
+            </li>
+            <li>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et
+              inventore ducimus dolores assumenda!
+            </li>
+            <li>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et
+              inventore ducimus dolores assumenda!
+            </li>
+          </ul>
+        </aside>
+        <main className='content bg-light'></main>
+      </div>
+    );
   }
 }
